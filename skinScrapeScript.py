c79c8e9684f909f2cb3a6d48c19dc2fa856b1662
d-parkin <parkindap@gmail.com>
2024-07-19 19:58:35 -0700

    Created script to scrape market history of skins

---
+++ skinScrapeScript.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from mysql.connector import Error
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Configure MySQL Databse
+db_config = {
+    "host": "**",
+    "user": "root",
+    "password": "**",
+    "database": "cs2",
+}
